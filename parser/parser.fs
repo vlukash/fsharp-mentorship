3ca941ab867b908331f0e9839fcb8545f39959b8
Volodymyr Lukashevych <v.lukashevych@gmail.com>
2016-11-01 00:45:18 -0700

    single character parser

---
+++ parser/parser.fs
@@ -0,0 +1,16 @@
+﻿namespace Parser
+
+type Result<'a> =
+     | Success of 'a
+     | Failure of string 
+
+type SingleCharParser = string -> Result<char>
+
+module SingleChar = 
+    open System
+
+    let parseChar str =
+        if String.IsNullOrEmpty(str) then
+            Failure "Input is empty"
+        else
+            Success(str.[0])
---
+++ parser/parser.tests.fs
@@ -0,0 +1,13 @@
+﻿namespace Parser
+
+module SingleCrar = 
+    open Xunit 
+    open SingleChar
+
+    [<Fact>]
+    let parse_non_empty_string_should_return_success_and_first_char() =
+        Assert.Equal(parseChar "abc", Success('a'))
+
+    [<Fact>]
+    let parse_empty_string_should_return_failure() =
+        Assert.Equal(parseChar "", Failure("Input is empty"))
