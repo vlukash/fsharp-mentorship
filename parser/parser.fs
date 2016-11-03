0c45bca9d0957c0e345957e497a685508af390ca
Volodymyr Lukashevych <v.lukashevych@gmail.com>
2016-11-02 23:05:02 -0700

    curried implementation to make 'parser factory'

--- parser/parser.fs
+++ parser/parser.fs
@@ -11,14 +11,17 @@ type SingleCharParser = string -> Result<char>
 module SingleChar = 
     open System
 
-    let parseFirstCharHardcodedA (input: string) (pos: int): Result<char> =
-        let charToMatch = 'a' // mrange: This can be a parameter obviously but even more powerful would be to have a test function parameter
-        if String.IsNullOrEmpty(input) then
-            Failure (["Input is empty"], 0)
-        else
-            let firstChar = input.[0]
-            if firstChar = charToMatch then
-                Success(charToMatch, pos+1)
+    // simple one char parser
+    let parseChar charToMatch = 
+        let parseCharInner input pos =
+            //let charToMatch = 'a' // mrange: This can be a parameter obviously but even more powerful would be to have a test function parameter
+            if String.IsNullOrEmpty(input) then
+                Failure (["Input is empty"], 0)
             else
-                let errorMessage = sprintf "Expected character is: '%c' but received '%c' at position %i" charToMatch firstChar pos
-                Failure ([errorMessage], pos)
+                let firstChar = input.[0]
+                if firstChar = charToMatch then
+                    Success(charToMatch, pos+1)
+                else
+                    let errorMessage = sprintf "Expected character is: '%c' but received '%c' at position %i" charToMatch firstChar pos
+                    Failure ([errorMessage], pos)
+        parseCharInner
--- parser/parser.tests.fs
+++ parser/parser.tests.fs
@@ -6,15 +6,18 @@ module SingleCrar =
 
     [<Fact>]
     let ``empty string should return failure and 0 as a pos value``() =
-        let result = parseFirstCharHardcodedA "" 0
+        let parser = parseChar 'a'
+        let result = parser "" 0
         Assert.Equal(result, Failure(["Input is empty"], 0))
 
     [<Fact>]
     let ``non empty string, first char is 'a', should return success and incremented position value``() =
-        let result = parseFirstCharHardcodedA "abc" 0
+        let parser = parseChar 'a'
+        let result = parser "abc" 0
         Assert.Equal(result, Success('a', 1))
 
     [<Fact>]
     let ``non empty string, first char is not 'a', should return failure``() =
-        let result = parseFirstCharHardcodedA "bca" 0
+        let parser = parseChar 'a'
+        let result = parser "bca" 0
         Assert.Equal(result, Failure(["Expected character is: 'a' but received 'b' at position 0"], 0))
