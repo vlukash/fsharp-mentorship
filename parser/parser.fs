6b243c51407e60e959149677c26629207c6aafa8
mrange <marten_range@hotmail.com>
2016-11-04 17:54:31 +0100

    1. Added type annotations 2. Added some comments

--- parser/parser.fs
+++ parser/parser.fs
@@ -1,7 +1,7 @@
-namespace Parser
+namespace ParserCombinator
 
 type Result<'T> =
-     | Success of 'T*int 
+     | Success of 'T*int
      | Failure of string list*int
 
 type Parser<'T> = string -> int -> Result<'T>
@@ -10,68 +10,69 @@ type Parser<'T> = string -> int -> Result<'T>
 // mrange: A parser is function that given a string and a position optionally produces a value (maybe a char) and a position
 // vlukash: removed
 
-module SingleChar = 
+module Parser =
     open System
 
     // simple one char parser
-    let parseChar charToMatch = 
-        let parseCharInner input pos =
-            //let charToMatch = 'a' // mrange: This can be a parameter obviously but even more powerful would be to have a test function parameter
-            if String.IsNullOrEmpty(input) then
-                Failure (["Input is empty"], pos)
-            else
-                let firstChar = input.[pos]
-                if firstChar = charToMatch then
-                    Success(charToMatch, pos+1)
-                else
-                    let errorMessage = sprintf "Expected character is: '%c' but received '%c' at position %i" charToMatch firstChar pos
-                    Failure ([errorMessage], pos)
-        parseCharInner
+    let singleChar charToMatch : Parser<char> =
+      fun input pos ->
+          //let charToMatch = 'a' // mrange: This can be a parameter obviously but even more powerful would be to have a test function parameter
+          if String.IsNullOrEmpty(input) then
+              Failure (["Input is empty"], pos)
+          else
+              let firstChar = input.[pos]
+              if firstChar = charToMatch then
+                  Success(charToMatch, pos+1)
+              else
+                  let errorMessage = sprintf "Expected character is: '%c' but received '%c' at position %i" charToMatch firstChar pos
+                  Failure ([errorMessage], pos)
+
 
     // combinator function that runs two parsers and returns the result as a pair
-    let pair l r input pos =
+    let pair (l : Parser<'L>) (r : Parser<'R>) : Parser<'L*'R> =
+      fun input pos ->
         // mrange: looks about right but see my comment below
         // call l parser
         match l input pos with
-            | Success (l_result, l_pos) -> 
+            | Success (l_result, l_pos) ->
                 // call r parser
                 match r input l_pos with
-                    | Success (r_result, r_pos) -> 
+                    | Success (r_result, r_pos) ->
                         Success ((l_result, r_result), r_pos) // returning combined result and updated position
-                    | Failure (err_msg, r_pos) -> 
-                        Failure (err_msg, l_pos) // mrange: I would use l_pos here      
-                                                 // vlukash: fixed, I've missed that names are the same in this result and in the first match                                    
-            | Failure (err_msg, l_pos) -> 
+                    | Failure (err_msg, r_pos) ->
+                        Failure (err_msg, r_pos) // mrange: I would use l_pos here
+                                                 // vlukash: fixed, I've missed that names are the same in this result and in the first match
+                                                 // mrange: My bad, it should be r_pos
+            | Failure (err_msg, l_pos) ->
                 Failure (err_msg, l_pos)
 
     // combinator function that takes two parsers and runs the first successful result
-    let orElse l r input pos = 
-        // call l parser
-        // mrange: Note my comments on the error results is difficult to resolve with our current parser type
-        // mrange: so I say ignore them but perhaps remember it for later.
-        match l input pos with
-            | Success (l_result, l_pos) -> 
-                Success (l_result, l_pos)                 
-            | Failure (l_err_msg, l_pos) -> 
-                // call r parser icase if r parser failed
-                match r input pos with // mrange: You want to use pos here over l_pos.
-                    | Success (r_result, r_pos) -> 
-                        Success (r_result, r_pos) // mrange: Here we are now discarding an error result which can be problematic
-                    | Failure (r_err_msg, pos) -> 
-                        Failure (l_err_msg @ r_err_msg, pos) // mrange: Actually here it would be correct to merge teh error results.
-                                                             // vlukash: concatenating two lists by using @
-                                                             // vlukash: but now for an empty input it'll return ["Input is empty";"Input is empty"]. But probably that's fine
+    let orElse (l : Parser<'T>) (r : Parser<'T>) : Parser<'T> =
+        fun input pos ->
+            // call l parser
+            // mrange: Note my comments on the error results is difficult to resolve with our current parser type
+            // mrange: so I say ignore them but perhaps remember it for later.
+            match l input pos with
+                | Success (l_result, l_pos) ->
+                    Success (l_result, l_pos)
+                | Failure (l_err_msg, l_pos) ->
+                    // call r parser icase if r parser failed
+                    match r input pos with // mrange: You want to use pos here over l_pos.
+                        | Success (r_result, r_pos) ->
+                            Success (r_result, r_pos) // mrange: Here we are now discarding an error result which can be problematic
+                        | Failure (r_err_msg, pos) ->
+                            Failure (l_err_msg @ r_err_msg, pos) // mrange: Actually here it would be correct to merge teh error results.
+                                                                 // vlukash: concatenating two lists by using @
+                                                                 // vlukash: but now for an empty input it'll return ["Input is empty";"Input is empty"]. But probably that's fine
+                                                                 // mrange: Merging two errors isn't as easy as this if the positions are different. Then a decent approximization could be
+                                                                 //         to use the error result for the parser that got furtherest
 
     // function that maps the result of parser into another type
-    let map converterFunc = 
-        let mapInner parser input pos =
-            match parser input pos with
-                | Success (result, s_pos) ->
-                    let convertedValue = converterFunc result
-                    Success (convertedValue, s_pos)
-                | Failure (err_msg, f_pos) ->
-                    Failure (err_msg, f_pos)
-        mapInner
-
-    // converts char to int
-    let converterFunc = fun ch -> string ch |> int
+    let map converterFunc (p : Parser<'T>) : Parser<'U> =
+      fun input pos ->
+          match p input pos with
+              | Success (result, s_pos) ->
+                  let convertedValue = converterFunc result
+                  Success (convertedValue, s_pos)
+              | Failure (err_msg, f_pos) ->
+                  Failure (err_msg, f_pos)
--- parser/parser.tests.fs
+++ parser/parser.tests.fs
@@ -1,32 +1,32 @@
-﻿namespace Parser
+﻿namespace ParserCombinator
 
 module SingleCrar = 
     open Xunit 
-    open SingleChar
+    open Parser
 
     [<Fact>]
     let ``empty string should return failure and 0 as a pos value``() =
-        let parser = parseChar 'a'
+        let parser = singleChar 'a'
         let result = parser "" 0
         Assert.Equal(result, Failure(["Input is empty"], 0))
 
     [<Fact>]
     let ``non empty string, first char is 'a', should return success and incremented position value``() =
-        let parser = parseChar 'a'
+        let parser = singleChar 'a'
         let result = parser "abc" 0
         Assert.Equal(result, Success('a', 1))
 
     [<Fact>]
     let ``non empty string, first char is not 'a', should return failure``() =
-        let parser = parseChar 'a'
+        let parser = singleChar 'a'
         let result = parser "bca" 0
         Assert.Equal(result, Failure(["Expected character is: 'a' but received 'b' at position 0"], 0))
 
     // combinator tests 
     [<Fact>]
     let ``combines two parsers and runs on the correct input string``() =
-        let parser_a = parseChar 'a'
-        let parser_b = parseChar 'b'
+        let parser_a = singleChar 'a'
+        let parser_b = singleChar 'b'
         let combinedParser = pair parser_a parser_b
 
         let result = combinedParser "abc" 0
@@ -34,8 +34,8 @@ module SingleCrar =
 
     [<Fact>]
     let ``first parser in the chain should fail``() =
-        let parser_a = parseChar 'a'
-        let parser_b = parseChar 'b'
+        let parser_a = singleChar 'a'
+        let parser_b = singleChar 'b'
         let combinedParser = pair parser_a parser_b
 
         let result = combinedParser "dbc" 0
@@ -43,8 +43,8 @@ module SingleCrar =
 
     [<Fact>]
     let ``second parser in the chain should fail``() =
-        let parser_a = parseChar 'a'
-        let parser_b = parseChar 'b'
+        let parser_a = singleChar 'a'
+        let parser_b = singleChar 'b'
         let combinedParser = pair parser_a parser_b
 
         let result = combinedParser "adc" 0
@@ -52,8 +52,8 @@ module SingleCrar =
 
     [<Fact>]
     let ``combinator should fail on empty input string``() =
-        let parser_a = parseChar 'a'
-        let parser_b = parseChar 'b'
+        let parser_a = singleChar 'a'
+        let parser_b = singleChar 'b'
         let combinedParser = pair parser_a parser_b
 
         let result = combinedParser "" 0
@@ -62,8 +62,8 @@ module SingleCrar =
     // orElse combinator tests 
     [<Fact>]
     let ``combines two parsers and runs only first one``() =
-        let parser_a = parseChar 'a'
-        let parser_b = parseChar 'b'
+        let parser_a = singleChar 'a'
+        let parser_b = singleChar 'b'
         let combinedParser = orElse parser_a parser_b
 
         let result = combinedParser "abc" 0
@@ -71,8 +71,8 @@ module SingleCrar =
 
     [<Fact>]
     let ``combines two parsers, first fails, second succeeded``() =
-        let parser_a = parseChar 'a'
-        let parser_b = parseChar 'b'
+        let parser_a = singleChar 'a'
+        let parser_b = singleChar 'b'
         let combinedParser = orElse parser_a parser_b
 
         let result = combinedParser "bdc" 0
@@ -80,8 +80,8 @@ module SingleCrar =
 
     [<Fact>]
     let ``both fail, error from second parser in the chain``() =
-        let parser_a = parseChar 'a'
-        let parser_b = parseChar 'b'
+        let parser_a = singleChar 'a'
+        let parser_b = singleChar 'b'
         let combinedParser = orElse parser_a parser_b
 
         let result = combinedParser "cde" 0
@@ -89,21 +89,24 @@ module SingleCrar =
 
     [<Fact>]
     let ``orElse combinator should fail on empty input string``() =
-        let parser_a = parseChar 'a'
-        let parser_b = parseChar 'b'
+        let parser_a = singleChar 'a'
+        let parser_b = singleChar 'b'
         let combinedParser = orElse parser_a parser_b
 
         let result = combinedParser "" 0
         Assert.Equal(result,  Failure(["Input is empty";"Input is empty"], 0))
 
+    let converterFunc c = int c - int '0'
+
     // map function tests 
     [<Fact>]
     let ``map function should convert char parser's result to int``() =
+
         // create char to int mapper
         let mapCharToInt = map converterFunc
 
         // create parser that parses char '2'
-        let parser_2 = parseChar '2'
+        let parser_2 = singleChar '2'
 
         // run parser and convert result to Int
         let result = mapCharToInt parser_2 "2abc" 0 
@@ -115,7 +118,7 @@ module SingleCrar =
         let mapCharToInt = map converterFunc
 
         // create parser that parses char '2'
-        let parser_2 = parseChar '2'
+        let parser_2 = singleChar '2'
 
         // run parser and convert result to Int
         let result = mapCharToInt parser_2 "abc2" 0 
