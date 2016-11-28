32ffcf9d5cebedadc27780b4e6f0d761a0c9e63a
Volodymyr Lukashevych (Globallogic Inc) <v-voluka@microsoft.com>
2016-11-28 13:23:05 -0800

    'bind' fixes

--- ComputationExpression/ComputationExpression.fs
+++ ComputationExpression/ComputationExpression.fs
@@ -8,10 +8,10 @@ type DivideBy<'T> = 'T -> Result<'T>
 
 module Result1 = 
     
-    //let log value = printfn "value is %A" value
+    let log value = printfn "value is %A" value
 
-    let divideBy top : DivideBy<int> = 
-        let intern bottom = 
+    let divideBy bottom : DivideBy<int> = 
+        let intern top = 
             if bottom = 0 then
                 Bad "Can't divide by 0"
             else 
@@ -20,33 +20,43 @@ module Result1 =
 
     // M<'T> -> ('T -> M<'U>) -> M<'U>
     let bind_ (div: DivideBy<'T>) (f: 'T -> DivideBy<'U>) : DivideBy<'U> =
-        let intern bottom = 
-            let result = div bottom
-            match result with
+        let intern top = 
+            let res = div top
+            match res with
                 | Ok value -> 
-                    f value bottom
-                | Bad err -> Bad err
+                    log value
+                    f top value
+                | Bad err -> 
+                    Bad err
         intern
         
     // 'T -> M<'T>
-    let return_ (x : 'T) : Result<'T> = 
-        let intern = 
+    let return_ (x : 'T) : DivideBy<'T> = 
+        let intern top = 
             Ok x
         intern
 
     type Builder() = 
-        member x.Bind(a, f) = 
+        member x.Bind(f, a) = 
             bind_ f a
  
         member x.Return(f) = 
             return_ f
 
+    let (>>=) f a = bind_ f a
+
     let res = new Builder()
 
     let div = res {
-        let! first =  divideBy
-        return first
+        let! first =  divideBy 5
+        let! second =  divideBy 2
+        let! third =  divideBy 3
+        return third
     }
 
-    let p = div 12
+    let d = div 60
+
+    let div2 = (divideBy 5) >>= (fun t -> divideBy 2 >>= (fun e -> return_ (e)))
+
+    let d2 = div2 60
 
