dd744e378f1c82b02cba5890d3ca40124425c444
Volodymyr Lukashevych <v.lukashevych@gmail.com>
2017-01-03 00:22:44 -0800

    workers receive data in big blocks instead of small per-line data

--- ParallelMandelbrot/AkkaNET.fs
+++ ParallelMandelbrot/AkkaNET.fs
@@ -3,6 +3,7 @@
 module AkkaNET = 
     open System
     open System.Collections.Concurrent
+    open System.Threading
     open MandelbrotTypes
 
     open Akka.Actor
@@ -13,10 +14,11 @@ module AkkaNET =
     open SingleThreaded
 
     let threadsCount = 4
+    let blockSizePerThread = 200
 
     type WorkerMsg = 
         | InitWorker
-        | CalculateBlock of ParallelTask
+        | CalculateBlock of ParallelTasksBlock
 
     type SupervisorMsg = 
         | RequestTask of IActorRef
@@ -35,12 +37,13 @@ module AkkaNET =
                     let requestTaskMsg : SupervisorMsg = RequestTask context.Self
                     context.Parent <! requestTaskMsg
                 | CalculateBlock blockParams ->
-                    let (ix, tx, ty, mx, my, maxIter, imageWidth) = blockParams
-                    // mandelbrot computation here
-                    let line = SingleThreaded.generateRowData ix tx ty mx my maxIter imageWidth 
-                    let resultData : SupervisorMsg = CalculatedResult line
-                    context.Parent <! resultData
-
+                    for blockParam in blockParams do
+                        let (ix, tx, ty, mx, my, maxIter, imageWidth) = blockParam
+                        // mandelbrot computation here
+                        let line = SingleThreaded.generateRowData ix tx ty mx my maxIter imageWidth 
+                        //Thread.Sleep(10)
+                        let resultData : SupervisorMsg = CalculatedResult line
+                        context.Parent <! resultData
                     let requestTaskMsg : SupervisorMsg = RequestTask context.Self
                     context.Parent <! requestTaskMsg
             | _ ->  failwith "unknown message"
@@ -76,16 +79,21 @@ module AkkaNET =
             | :? SupervisorMsg as msg -> 
                 match msg with
                 | RequestTask actorRef->
-                    let (success, res) = taskQueue.TryDequeue()
-                    if success then 
-                        let task : WorkerMsg = CalculateBlock res
-                        actorRef <! task
-                    else 
-                        //shutdown current worker
+                    // shutdown current worker if taskQueue is empty
+                    if taskQueue.IsEmpty then
                         actorRef <! PoisonPill.Instance
                         activeWorkersCount <- activeWorkersCount - 1
                         if activeWorkersCount = 0 then
                             completed ()
+                    else 
+                        let parallelTasksBlock : ParallelTasksBlock = [
+                            for i in 1 .. blockSizePerThread do
+                                let (success, res) = taskQueue.TryDequeue()
+                                if success then
+                                    yield res 
+                        ]
+                        let task : WorkerMsg = CalculateBlock parallelTasksBlock
+                        actorRef <! task
                 | CalculatedResult result ->
                     let line : Line = result
                     enqueue line
--- ParallelMandelbrot/MandelbrotTypes.fs
+++ ParallelMandelbrot/MandelbrotTypes.fs
@@ -9,5 +9,8 @@ type ComputationType =
 // ix tx ty mx my maxIter imageWidth
 type ParallelTask = int * float * float * float * float * int * int
 
+// Block of data to be calculated on any worker
+type ParallelTasksBlock = ParallelTask list
+
 // ix byte array
 type ParallelTaskResult = int * byte array
